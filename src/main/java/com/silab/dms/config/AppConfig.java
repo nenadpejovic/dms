7cf6e868f87b4ea15e12a22e863231fac4823ce9
Marko Savic <Marko.Savic@zuhlke.com>
2017-02-05 18:05:59 +0100

    update config for CORS

--- src/main/java/com/silab/dms/config/AppConfig.java
+++ src/main/java/com/silab/dms/config/AppConfig.java
@@ -9,15 +9,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- *
  * @author PredragiNenad
  */
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.silab.dms")
-public class AppConfig {
-    
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
