e1dee2f256bff7d21337e15616d4dfc49fe0997a
Marko Savic <Marko.Savic@zuhlke.com>
2017-02-05 18:06:23 +0100

    add roles for users

---
+++ src/main/java/com/silab/dms/Utils/Role.java
@@ -0,0 +1,8 @@
+package com.silab.dms.Utils;
+
+/**
+ * Created by msav on 2/5/2017.
+ */
+public enum Role {
+    ADMIN, EMPLOYEE;
+}
--- src/main/java/com/silab/dms/model/User.java
+++ src/main/java/com/silab/dms/model/User.java
@@ -13,106 +13,117 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.silab.dms.Utils.Role;
 import org.hibernate.validator.constraints.Range;
 
 @Entity
-@Table(name="user")
+@Table(name = "user")
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
 
-	@Column(name="firstName")
-	@Range(min = 2)
-	private String firstName;
+    @Column(name = "firstName")
+    @Range(min = 2)
+    private String firstName;
 
-	@Column(name="lastName")
-	@Range(min = 2)
-	private String lastName;
+    @Column(name = "lastName")
+    @Range(min = 2)
+    private String lastName;
 
-	@Column(name="username")
-	private String username;
+    @Column(name = "username")
+    private String username;
 
-	@Column(name="password")
-	private String password;
+    @Column(name = "password")
+    private String password;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "companyId", referencedColumnName="companyId", nullable = true)
-	private Company company;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "companyId", referencedColumnName = "companyId", nullable = true)
+    private Company company;
 
-	public int getId() {
-		return id;
-	}
+    private Role role;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 
-	public Company getCompany() {
-		return company;
-	}
+    public Company getCompany() {
+        return company;
+    }
 
-	public void setCompany(Company company) {
-		this.company = company;
-	}
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+    public Role getRole() {
+        return role;
+    }
 
-		User user = (User) o;
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
-		if (getId() != user.getId()) return false;
-		if (!getUsername().equals(user.getUsername())) return false;
-		return getPassword().equals(user.getPassword());
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-	}
+        User user = (User) o;
 
-	@Override
-	public int hashCode() {
-		int result = getId();
-		result = 31 * result + getUsername().hashCode();
-		result = 31 * result + getPassword().hashCode();
-		return result;
-	}
+        if (getId() != user.getId()) return false;
+        if (!getUsername().equals(user.getUsername())) return false;
+        return getPassword().equals(user.getPassword());
 
-	@Override
-	public String toString() {
-		return firstName + " " + lastName + " (" + company + ")";
-	}
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + getUsername().hashCode();
+        result = 31 * result + getPassword().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " (" + company + ")";
+    }
 }
